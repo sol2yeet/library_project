243242d5c4ac6f3b4b863d9e8b6b8618a87c3fe6
Yesol <hoy1342@gmail.com>
2023-03-15 16:18:55 +0900

    add text file message

--- library_v2.0/RENT.h
+++ library_v2.0/RENT.h
@@ -4,7 +4,7 @@
 
 void rent_BOOK();    // 대출
 void return_BOOK();  // 반납
-void rent_tot();     // 연체 현황
+void rent_list();    // 연체 현황
 void input_RENT();   // 대여정보 입력
 void input_RETURN(); // 반납정보 입력
 
